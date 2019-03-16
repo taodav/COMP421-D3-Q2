5baaf931c386de7c8b31976b6ad5d1d3129599a2
eliottbourachot <eliott.bourachot@gmail.com>
2019-03-16 12:21:19 -0400

    Added initial prompt

--- src/ConsoleSQL.java
+++ src/ConsoleSQL.java
@@ -13,17 +13,45 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
-class simpleJDBC
+class ConsoleSQL
 {
-	enum State {Start};
+	enum State {Start, Loan_Copy, Return_Copy, View_Loans_Holds, Check_Available, Check_Section};
 	static State state = State.Start;
 	
 	public static void main(String args[]) throws SQLException
 	{
+		System.out.println("Welcome to the Group 38 SQL Console 3000");
+		
 		try {
 			switch(state) {
 			case Start:
+				System.out.println("Please select one of the following options by entering it's corresponding number");
+				System.out.println("1. Loan a copy");
+				System.out.println("2. Return a copy");
+				System.out.println("3. View the loans and holds of a copy");
+				System.out.println("4. Check the availability of a Book/Periodical");
+				System.out.println("5. Check the section of a Book/Periodical");
+				Scanner scanner = new Scanner(System. in);
+		        switch(scanner.nextLine()) {
+		        case "1": state = State.Loan_Copy; break;
+		        case "2": state = State.Return_Copy; break;
+		        case "3": state = State.View_Loans_Holds; break;
+		        case "4": state = State.Check_Available; break;
+		        case "5": state = State.Check_Section; break;
+		        default: System.out.println("Invalid selection. Please try again"); break;
+		        }
+				break;
+			case Loan_Copy:
+				break;
+			case Return_Copy:
+				break;
+			case View_Loans_Holds:
+				break;
+			case Check_Available:
+				break;
+			case Check_Section:
 				break;
 			default:
 				break;
